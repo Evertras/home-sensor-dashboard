12820232142be5fc7eec57e32ea38b6090384f74
Brandon Fulljames <bfullj@gmail.com>
2022-10-23 23:56:29 +0900

    Lint tweak for spacing

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -1,7 +1,8 @@
 module "lambda_dummy" {
   source = "./modules/lambda"
 
-  name                      = "dummy"
-  code                      = file("${path.module}/../lambdas/dummy.js")
+  name = "dummy"
+  code = file("${path.module}/../lambdas/dummy.js")
+
   api_gateway_execution_arn = aws_apigatewayv2_api.api.execution_arn
 }
