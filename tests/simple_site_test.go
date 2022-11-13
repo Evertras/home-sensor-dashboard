50890443a8c9e275f0d36a08b5e3a83b0f209a73
Brandon Fulljames <bfullj@gmail.com>
2022-11-13 10:31:32 +0900

    Quick test for simple site showing data

--- tests/simple_site_test.go
+++ tests/simple_site_test.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"time"
+
+	"github.com/cucumber/godog"
 )
 
 type simpleSiteGetter interface {
@@ -20,6 +24,33 @@ func (t *testContext) iRequestTheSimpleSite() error {
 
 	if err != nil {
 		return fmt.Errorf("t.provider.GetSimpleSite: %w", err)
+	}
+
+	return nil
+}
+
+func (t *testContext) thePageShouldContainATableWithRows(data *godog.Table) error {
+	// TODO: should just store body on read, but for now...
+	raw, err := io.ReadAll(t.lastResponse.Body)
+
+	if err != nil {
+		return fmt.Errorf("failed to read body: %w", err)
+	}
+
+	body := string(raw)
+	notFound := []string{}
+
+	// TODO: Parsing HTTP body is out of scope, just bare minimum check for now
+	for _, row := range data.Rows {
+		for _, cell := range row.Cells {
+			if !strings.Contains(body, cell.Value) {
+				notFound = append(notFound, cell.Value)
+			}
+		}
+	}
+
+	if len(notFound) > 0 {
+		return fmt.Errorf("missing expected values: %s", strings.Join(notFound, ", "))
 	}
 
 	return nil
