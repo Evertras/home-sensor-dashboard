f79c64cd9a56c4194562b8e3221703f859fcdbcb
Brandon Fulljames <bfullj@gmail.com>
2022-11-13 10:49:15 +0900

    Simple table view

--- lambdas/simple_site.js
+++ lambdas/simple_site.js
@@ -12,8 +12,52 @@ module.exports.handler = async () => {
   // Just let errors happen if they happen...
   const allMeasurements = data.Items;
 
+  const measurementBySensor = {};
+
+  for (let data of allMeasurements) {
+    if (!measurementBySensor[data.SensorID]) {
+      measurementBySensor[data.SensorID] = {};
+    }
+
+    measurementBySensor[data.SensorID][data.MeasurementType] =
+      data.MeasurementValue;
+  }
+
+  const rows = Object.keys(measurementBySensor).map((id) => {
+    const data = measurementBySensor[id];
+    const tempC = data.temperaturec;
+    const tempF = Math.floor(tempC * 1.8 + 32);
+
+    return `<tr><td>${id}</td><td>${tempC}</td><td>${tempF}</td><td>${
+      data.humidity100 || "N/A"
+    }%</td></tr>`;
+  });
+
+  const table = `
+<table>
+  <tr>
+    <th>Location</th>
+    <th>Temperature (C)</th>
+    <th>Temperature (F)</th>
+    <th>Humidity (100%)</th>
+  </tr>
+  ${rows.join("\n")}
+</table>
+`;
+
+  const page = `
+<html>
+  <body>
+    ${table}
+  </body>
+</html>
+  `;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ value: data }),
+    body: page,
+    headers: {
+      "Content-type": "text/html",
+    },
   };
 };
